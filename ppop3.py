4ae38b102e165ac33b8925d095cb659d5bba450c
Chekoo <a316296241@163.com>
2017-06-11 17:04:20 +0800

    learn

---
+++ ppop3.py
@@ -0,0 +1,62 @@
+# coding=utf-8
+# 解析邮件
+
+
+from email.parser import Parser
+from email.header import decode_header
+from email.utils import parseaddr
+import poplib
+
+
+msg = Parser().parsestr(msg_content)
+
+
+# indent用于缩进显示
+def print_info(msg, indent=0):
+    if indent == 0:
+        for header in ['From', 'To', 'Subject']:
+            value = msg.get(header, '')
+            if value:
+                if header == 'Subject':
+                    value = decode_str(value)
+                else:
+                    hdr, addr = parseaddr(value)
+                    name = decode_str(hdr)
+                    value = u'%s <%s>' % (name, addr)
+            print('%s%s: %s' % (' ' * indent, header, value))
+    if(msg.is_multipart()):
+        parts = msg.get_payload()
+        for n, part in enumerate(parts):
+            print('%spart %s' % (' ' * indent, n))
+            print('%s------------------' % (' ' * indent))
+            print_info(part, indent + 1)
+    else:
+        content_type = msg.get_content_type()
+        if content_type=='text/plain' or content_type=='text/html':
+            content = msg.get_payload(decode=True)
+            charset = guess_charset(msg)
+            if charset:
+                content = content.decode(charset)
+            print('%sText: %s' % (' ' * indent, content + '...'))
+        else:
+            print('%sAttachment: %s' % (' ' * indent, content_type))
+
+
+# 邮件的Subject或Email包含的名字都是经过编码后的str,要正常显示，就要decode
+# decode_header()返回一个list
+def decode_str(s):
+    value, charset = decode_header(s)[0]
+    if charset:
+        value = value.decode(charset)
+    return value
+
+
+# 
+def guess_charset():
+    charset = msg.get_charset()
+    if charset is None:
+        content_type = msg.get('Content-Type', '').lower()
+        pos = content_type.find('charset=')
+        if pos >= 0:
+            charset = content_type[pos + 8:].strip()
+    return charset
